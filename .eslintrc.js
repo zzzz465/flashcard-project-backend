00fee1d5419cc81896457bcfee9a03f0b25da290
madeline <zzzz465@naver.com>
2021-02-14 17:17:29 +0900

    swagger UI 설정
    index.js 의 45번째 줄을 참고, swaggerUI.serve 사용방법

---
+++ .eslintrc.js
@@ -0,0 +1,17 @@
+/* eslint-disable no-undef */
+module.exports = {
+  root: true,
+  parser: "@typescript-eslint/parser",
+  plugins: [
+    "@typescript-eslint"
+  ],
+  extends: [
+    "eslint:recommended",
+    "plugin:@typescript-eslint/eslint-recommended",
+    "plugin:@typescript-eslint/recommended"
+  ],
+
+  rules: {
+    "@typescript-eslint/indent": ["warn", 2]
+  }
+}
--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,50 @@
-import App from 'express'
+import App, { json } from 'express'
+import swaggerUI from 'swagger-ui-express'
+import swaggerJsDoc, { Options, SwaggerDefinition } from 'swagger-jsdoc'
+import cookieParser from 'cookie-parser'
 
 const app = App()
+app.use(json())
+app.use(cookieParser())
 
+const swaggerDefinition: SwaggerDefinition = {
+  info: {
+    title: 'flashcard-project-API-Documentation',
+    version: '1.0.0',
+  }
+}
+
+const swaggerOptions: Options = {
+  swaggerDefinition,
+  apis: ['./**/*.ts']
+}
+
+
+/**
+ * @swagger
+ * /todo:
+ *   get:
+ *     summary: Returns Todo list
+ *     tags: [Todo]
+ *     responses:
+ *       200:
+ *         description: todo list
+ *         schema:
+ *           type: object
+ *           properties:
+ *             todos:
+ *               type: array
+ *               items:
+ *                 $ref: '#/definitions/Todo'
+ */
 app.get('/', (req, res) => {
-    res.send('hello world!').end()
+  res.send('hello world!').end()
 })
 
+const spec = swaggerJsDoc(swaggerOptions)
+app.use('/', swaggerUI.serve)
+app.get('/api-docs', swaggerUI.setup(spec))
+
 app.listen(80, () => {
-    console.log('server listening on port 80')
+  console.log('server listening on port 80')
 })
