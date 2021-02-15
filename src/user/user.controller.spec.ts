c11566fc10f5828d6d04dff720bf0de2b61a8aa3
madeline <zzzz465@naver.com>
2021-02-15 23:13:38 +0900

    Unit test 코드 수정

--- src/user/user.controller.spec.ts
+++ src/user/user.controller.spec.ts
@@ -1,5 +1,7 @@
 import { Test, TestingModule } from '@nestjs/testing'
 import { UserController } from './user.controller'
+import { UserRepository } from './user.repository'
+import { UserService } from './user.service'
 
 describe('UserController', () => {
   let controller: UserController
@@ -7,6 +9,7 @@ describe('UserController', () => {
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [UserController],
+      providers: [UserService, UserRepository]
     }).compile()
 
     controller = module.get<UserController>(UserController)
