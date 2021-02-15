b9ea831dbf8a6e42584b6ac7e2f4450301889aef
madeline <zzzz465@naver.com>
2021-02-15 21:32:31 +0900

    passport 라이브러리 추가, eslint 변경

--- .eslintrc.js
+++ .eslintrc.js
@@ -6,9 +6,11 @@ module.exports = {
   },
   plugins: ['@typescript-eslint/eslint-plugin'],
   extends: [
+    "eslint:recommended",
+    "plugin:@typescript-eslint/eslint-recommended",
     'plugin:@typescript-eslint/recommended',
-    'prettier/@typescript-eslint',
     'plugin:prettier/recommended',
+    'prettier/@typescript-eslint',
   ],
   root: true,
   env: {
@@ -21,5 +23,7 @@ module.exports = {
     '@typescript-eslint/explicit-function-return-type': 'off',
     '@typescript-eslint/explicit-module-boundary-types': 'off',
     '@typescript-eslint/no-explicit-any': 'off',
+    'curly': ['warn', 'multi-or-nest'],
+    'semi': ['warn', 'never']
   },
 };
